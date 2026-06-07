249bd3050a6aa8510beb82815eaf09257a3a7f82
FSD <antiperhenryotieno@gmail.com>
2026-06-07 17:16:58 +0300

    autocommitted [2026-06-07 17:16]

--- server/strategies/index.ts
+++ server/strategies/index.ts
@@ -1,10 +1,7 @@
 import { strategyRegistry } from './core/strategyRegistry';
-import { smcStrategy } from './smc';
 
 export function initializeStrategies(): void {
   console.log('[Strategies] Initializing trading strategies...');
-
-  strategyRegistry.register(smcStrategy);
 
   const stats = strategyRegistry.getStats();
   console.log(`[Strategies] Registered ${stats.totalStrategies} strategies (${stats.enabledStrategies} enabled)`);
@@ -13,5 +10,3 @@ export function initializeStrategies(): void {
 export { strategyRegistry } from './core/strategyRegistry';
 export { BaseStrategy } from './core/baseStrategy';
 export * from './core/types';
-
-export { smcStrategy } from './smc';
