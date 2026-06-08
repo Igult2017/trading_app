406f7ad09f10263b6e6eb4cd502726c3506b02ba
FSD <antiperhenryotieno@gmail.com>
2026-06-08 23:44:52 +0300

    autocommitted [2026-06-08 23:44]

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,17 @@
 import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
+import { I18nextProvider } from "react-i18next";
 import { LanguageProvider } from "./context/LanguageContext";
+import i18n from "./i18n/index";
 import App from "./App";
 import "./index.css";
-import "./i18n/index";
 
 createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
-    <LanguageProvider>
-      <App />
-    </LanguageProvider>
+    <I18nextProvider i18n={i18n}>
+      <LanguageProvider>
+        <App />
+      </LanguageProvider>
+    </I18nextProvider>
   </HelmetProvider>
 );
